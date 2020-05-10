fd3febd5f3560d45f74e77b4f059ccc436ec0a7b
Hugh Wells <hugh@crablab.co.uk>
2020-05-10 17:36:44 +0100

    Document the YouTube class properly
    Somehow I forgot the .gitignore, whoops

--- youtube.py
+++ youtube.py
@@ -26,6 +26,12 @@ class youtube:
     ## METHODS
 
     def get_subtitles(self, video_id):
+        """
+        Orchestrates the download of the subtitle file.
+
+        :param video_id: The YouTube video ID to grab subtitles for
+        :returns: URL to the downloaded file
+        """
 
         tracks = self._list_subtitle_tracks(video_id)
 
@@ -34,8 +40,17 @@ class youtube:
         for track in tracks:
             subtitle_files.append(self._get_track(video_id, track))
         
+        return subtitle_files
 
     def _list_subtitle_tracks(self, video_id):
+        """
+        Grabs the subtitle tracks (usually just one) for the video. 
+        Note: by default this will return the subtitle files in the uploaded language. 
+
+        :param video_id: YouTube video ID to get the tracks for
+        :returns: List of subtitle track IDs
+        """ 
+
         r = requests.get("https://www.googleapis.com/youtube/v3/captions/", params={"key": self._key, "videoId": video_id, "part": "id"})
 
         ids = []
@@ -46,6 +61,11 @@ class youtube:
         return ids
 
     def _get_track(self, video_id, subtitle_id):
+        """
+        Downloads the subtitle file for a given track. Saved in `tmp` with the format `video_id.track_id`. 
+        Requires OAuth (groan) and will initiate that if required.
+        """
+
         # Get OAuth if needed
         if(self._check_oauth() == False):
             self._get_access_token()
@@ -62,6 +82,11 @@ class youtube:
         return filename
 
     def _check_oauth(self):
+        """
+        Checks if an OAuth token was loaded and if it has expired.
+
+        :returns: Boolean success factor
+        """
         if(self._token == None):
             return False
         
@@ -71,6 +96,17 @@ class youtube:
         return True
 
     def _get_access_token(self):
+        """
+        Does all the OAuth magic. 
+
+        Will:
+        - Start an OAuth2 session
+        - Pass on the auth URL 
+        - Take the redirect URL and then grab the token 
+        - Set the token in the class and write the JSON to `tmp/oauth` for later
+        
+        :returns: Boolean success factor
+        """
         scope = ['https://www.googleapis.com/auth/youtube.force-ssl']
 
         self._oauth = OAuth2Session(self._id, scope=scope, redirect_uri="https://localhost")
